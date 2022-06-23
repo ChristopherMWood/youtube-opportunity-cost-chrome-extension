1890fbe6a4157d5845b3fe1ff9244f606a14aace
Christopher Wood <cmw2379@gmail.com>
2022-06-23 13:59:34 -0400

    Added carousel and icons

--- src/action/index.js
+++ src/action/index.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
             const videoId = getVideoIdFromUrl(activeTab.url);
 
             chrome.storage.local.get([videoId], function(result) {
-                // alert('Value currently is ' + JSON.stringify(result[videoId].data));
                 setValuesInUI(result[videoId].data);
                 document.getElementById("site_link").href = "https://opportunitycost.life?v=" + videoId;
             });
@@ -18,12 +17,72 @@ document.addEventListener("DOMContentLoaded", function(event) {
      });
 });
 
-function setValuesInUI(data) {
+function roundUp(num, precision) {
+    precision = Math.pow(10, precision);
+    return Math.ceil(num * precision) / precision;
+}
+
+function getLivesLost(data) {
     const averageLifeSpanInYears = 73.2;
     const yearsWatched = data.totalSeconds/31557600
     const averageLivesLost =  yearsWatched/averageLifeSpanInYears;
+    const precision = averageLivesLost > 1 ? 2 : 4;
+    return roundUp(averageLivesLost, precision);
+}
 
-    document.getElementById("lives-spent-digit").innerHTML = averageLivesLost.toPrecision(5);
+function getYearsLostString(data) {
+    const yearsWatched = data.totalSeconds/31557600
+
+    if (yearsWatched > 1000) {
+        return getShortFormatString(yearsWatched);
+    }
+
+    const precision = yearsWatched > 1 ? 2 : 4;
+    return String(roundUp(yearsWatched, precision));
+}
+
+function getCollegeEducationsString(data) {
+    const yearsWatched = data.totalSeconds/31557600;
+    const collegeEducations = yearsWatched/4;
+
+    if (collegeEducations > 1000) {
+        return getShortFormatString(collegeEducations);
+    }
+
+    const precision = collegeEducations > 1 ? 2 : 4;
+    return String(roundUp(collegeEducations, precision));
+}
+
+function getWeekendsString(data) {
+    const weekendsWatched = data.totalSeconds/(86400 * 2);
+
+    if (weekendsWatched > 1000) {
+        return getShortFormatString(weekendsWatched);
+    }
+
+    const precision = weekendsWatched > 1 ? 2 : 4;
+    return String(roundUp(weekendsWatched, precision));
+}
+
+function getShortFormatString(value) {
+    if (value > 1000000) {
+        const millions = value/1000000;
+        const milResults = millions + (value - (millions * 1000000));
+        return String(roundUp(milResults, 2) + 'mil');
+    } else if (value > 1000) {
+        const thousands = value/1000;
+        const kResults = thousands + (value - (thousands * 1000));
+        return String(roundUp(kResults, 1) + 'k');
+    }
+
+    return String(value);
+}
+
+function setValuesInUI(data) {
+    document.getElementById("lives-spent-digit").innerHTML = getLivesLost(data);
+    document.getElementById("years-spent-digit").innerHTML = getYearsLostString(data);
+    document.getElementById("college-educations-spent-digit").innerHTML = getCollegeEducationsString(data);
+    document.getElementById("weekends-spent-digit").innerHTML = getWeekendsString(data);
 }
 
 const YouTubeUrlFormatOne = /^https?:\/\/(?:youtu\.be|(?:www\.)?youtube\.com\/embed)\/([\w\-]+)/;
